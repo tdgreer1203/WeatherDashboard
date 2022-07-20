fdf56d8719cd911dd5a95f1a52980692d4de48f2
Theo <tdgreer1203@gmail.com>
2022-07-20 16:24:26 -0400

    Working on Current Weather Screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,43 +14,99 @@ var iconUrl = "";
 var lon = "";
 var lat = "";
 var apiUrl = "";
+
+iconUrl = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png";
+                    tempEl.val(data.current.temp);
+                    console.log(data.current.temp);
+                    windEl.val(data.current.wind_speed);
+                    humidityEl.val(data.current.humidity);
+                    uvindexEl.val(data.current.uvi);
 */
 //data.main.temp
 
 $(document).ready(function(){
     var apiKey = "&appid=011fc9a6ee8731e0d02abfa9e7a65489"; 
+    var apiUrl = "";
     var inputEl = $('#city-input');
     var buttonEl = $('#search-button');  
     var modalMessage = $('#modal-message');
-    
+    var cityTitleEl = $('#city-title');
+    var tempEl = $('#current-temp');
+    var windEl = $('#current-wind');
+    var humidityEl = $('#current-humidity');
+    var uvindexEl = $('#current-uvindex');
+    var iconEl = $('#city-title-icon');
+    var iconUrl = "";
+    var temp;
+    var humidity;
+    var wind;
+    var uvi;
+    var city;
+    var date = new Date().toLocaleDateString('en-US');
+
     $('.modal').modal();
     
-    var generateUrl = function(city) {
-        var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&exclude=minutely,hourly,daily,alerts" + apiKey;
-        var response = fetch(apiUrl).then(function(response){
-            response.json().then(function(data) {
-                apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=minutely,hourly,alerts&units=imperial" + apiKey;
-            });
+    function generateUrl(city) {
+        apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&exclude=minutely,hourly,daily,alerts" + apiKey;
+        let response = fetch(apiUrl).then(function(response){
+            if(response.ok) {
+                response.json().then(function(data) {
+                    apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=minutely,hourly,alerts&units=imperial" + apiKey;
+                }).then(function() {
+                    setValues(apiUrl);
+                });
+            } else {
+                showModal("There was a problem with the request. Please try again. Error Code: " + response.status);
+            }
         });
-        return apiUrl;
+        inputEl.val("");
+    }
+
+    function setValues(apiUrl) {
+        let response = fetch(apiUrl).then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    //Start populating data fields
+                    temp = data.current.temp;
+                    humidity = data.current.humidity;
+                    wind = data.current.wind_speed;
+                    uvi = data.current.uvi;
+                    iconUrl = data.current.weather[0].icon;
+                }).then(function() {
+                    populateCurrentScreen();
+                });
+            } else {
+                showModal("There was a problem with the request. Please try again. Error Code: " + response.status);
+            }
+        });
+        inputEl.val("");
+    }
+
+    function populateCurrentScreen() {
+        var img = $('<img>');
+        img.attr('src', "http://openweathermap.org/img/wn/" + iconUrl + ".png");
+        img.append(iconEl);
+        cityTitleEl.text(city + " " + date);
+        tempEl.text(temp + "\u00B0F");
+        windEl.text(wind + " MPH");
+        humidityEl.text(humidity + "%");
+        uvindexEl.text(uvi);
     }
     
-    var showModal = function(message) {
+    function showModal(message) {
             var myModal = M.Modal.getInstance($('.modal'));
+            inputEl.val("");
             modalMessage.text(message);
             myModal.open();
     }
 
     buttonEl.click(function() {
-        var city = inputEl.val();
+        city = inputEl.val().trim();
         if(city) {
             generateUrl(city);
         } else {
             showModal("Oh no! It looks like you forgot to enter a city. Please enter a city.");
         }
-    });
-
-
-    console.log(generateUrl("Dallas"));
+    });    
 });
 
