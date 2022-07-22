d18da4836dc7a82aea6dd6f37f403ee5d7691da2
Theo <tdgreer1203@gmail.com>
2022-07-22 07:17:18 -0400

    Updated CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    var apiKey = "&appid=011fc9a6ee8731e0d02abfa9e7a65489"; 
-    var apiUrl = "";
+    const apiKey = "&appid=011fc9a6ee8731e0d02abfa9e7a65489"; 
+
     var inputEl = $('#city-input');
     var buttonEl = $('#search-button');  
     var modalMessage = $('#modal-message');
@@ -9,7 +9,10 @@ $(document).ready(function(){
     var windEl = $('#current-wind');
     var humidityEl = $('#current-humidity');
     var uvindexEl = $('#current-uvindex');
-    var iconEl = $('#city-title-icon');
+    var fiveDayForecastEl = $('#five-day-forecast');
+    var previousSearchesEl = $('#previous-searches');
+
+    var apiUrl = "";
     var iconUrl = "";
     var icon;
     var temp;
@@ -19,6 +22,7 @@ $(document).ready(function(){
     var city;
     var date = new Date().toLocaleDateString('en-US');
     var fiveDayForecast = [];
+    var searchHistory = [];
 
     $('.modal').modal();
     
@@ -72,8 +76,66 @@ $(document).ready(function(){
         windEl.text(wind + " MPH");
         humidityEl.text(humidity + "%");
         uvindexEl.text(uvi);
+
+        addToHistory();
     }
     
+    function populateFiveDayScreen() {
+        date = new Date();
+        for(var i = 0; i < fiveDayForecast.length; i++) {
+            icon = fiveDayForecast[i].weather[0].icon;
+            temp = fiveDayForecast[i].temp.day;
+            wind = fiveDayForecast[i].wind_speed;
+            humidity = fiveDayForecast[i].humidity
+            iconUrl = "http://openweathermap.org/img/wn/" + icon + ".png";
+            date.setDate(date.getDate() + 1);
+
+            var divColumn = $('<div>').addClass("col s6 m4 l2 single-day-forecast white black-text center forecast");
+            var title = $('<h5>').addClass("flow-text").text(date.toLocaleDateString('en-US'));
+            var image = $('<img>').attr('src', iconUrl);
+            var pTemp = $('<p>').text("Temp: " + temp + "\u00B0F");
+            var pWind = $('<p>').text("Wind: " + wind + " MPH");
+            var pHumidity = $('<p>').text("Humidity: " + humidity + "%");
+
+            divColumn.append(title);
+            divColumn.append(image);
+            divColumn.append(pTemp);
+            divColumn.append(pWind);
+            divColumn.append(pHumidity);
+
+            fiveDayForecastEl.append(divColumn);
+        }
+
+        saveSearch();
+    }
+
+    function addToHistory() {
+        var prevBtn = $('<a>').addClass("waves-effect waves-light btn white black-text hoverable").text(city);
+        previousSearchesEl.append(prevBtn);
+    }
+
+    function saveSearch() {
+        searchHistory.push(city);
+        localStorage.setItem("searches", JSON.stringify(searchHistory));
+    }
+
+    function populateSearches() {
+        for(var i = 0; i < searchHistory.length; i++) {
+            var prevBtn = $('<a>').addClass("waves-effect waves-light btn white black-text hoverable").text(searchHistory[i]);
+            previousSearchesEl.append(prevBtn);
+        }
+    }
+
+    function loadSearches() {
+        var savedSearches = localStorage.getItem("searches");
+        if(!savedSearches) {
+            searchHistory = [];
+        } else {
+            searchHistory = JSON.parse(savedSearches);
+        }
+        populateSearches();
+    }
+
     function showModal(message) {
             var myModal = M.Modal.getInstance($('.modal'));
             inputEl.val("");
@@ -81,13 +143,19 @@ $(document).ready(function(){
             myModal.open();
     }
 
+    function clearForecast() {
+        fiveDayForecastEl.empty();
+    }
+
     buttonEl.click(function() {
         city = inputEl.val().trim();
         if(city) {
+            clearForecast();
             generateUrl(city);
         } else {
             showModal("Oh no! It looks like you forgot to enter a city. Please enter a city.");
         }
-    });    
-});
+    });   
 
+    loadSearches();
+});
